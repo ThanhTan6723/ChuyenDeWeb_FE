da6f7a910dd4a0ee79b06f5559f2d0642b3c4441
NguyenQuocTrungg <21130588@st.hcmuaf.edu.vn>
2025-06-15 16:59:11 +0700

    add product admin

--- src/admin/components/layout/Footer.js
+++ src/admin/components/layout/Footer.js
@@ -4,16 +4,16 @@ export default function Footer() {
     return (
         <footer className="content-footer footer bg-footer-theme">
             <div className="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-                <div className="mb-2 mb-md-0">
-                    © {new Date().getFullYear()}, made with ❤️ by
-                    <a href="https://themeselection.com" target="_blank" rel="noopener noreferrer" className="footer-link fw-bolder"> ThemeSelection</a>
-                </div>
-                <div>
-                    <a href="https://themeselection.com/license/" className="footer-link me-4" target="_blank" rel="noopener noreferrer">License</a>
-                    <a href="https://themeselection.com/" className="footer-link me-4" target="_blank" rel="noopener noreferrer">More Themes</a>
-                    <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/" className="footer-link me-4" target="_blank" rel="noopener noreferrer">Documentation</a>
-                    <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" className="footer-link me-4" target="_blank" rel="noopener noreferrer">Support</a>
-                </div>
+                {/*<div className="mb-2 mb-md-0">*/}
+                {/*    © {new Date().getFullYear()}, made with ❤️ by*/}
+                {/*    <a href="https://themeselection.com" target="_blank" rel="noopener noreferrer" className="footer-link fw-bolder"> ThemeSelection</a>*/}
+                {/*</div>*/}
+                {/*<div>*/}
+                {/*    <a href="https://themeselection.com/license/" className="footer-link me-4" target="_blank" rel="noopener noreferrer">License</a>*/}
+                {/*    <a href="https://themeselection.com/" className="footer-link me-4" target="_blank" rel="noopener noreferrer">More Themes</a>*/}
+                {/*    <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/" className="footer-link me-4" target="_blank" rel="noopener noreferrer">Documentation</a>*/}
+                {/*    <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" className="footer-link me-4" target="_blank" rel="noopener noreferrer">Support</a>*/}
+                {/*</div>*/}
             </div>
         </footer>
     );
